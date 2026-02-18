9259a55a8f7530de73983a8c23a902b2413a8fef
DHARUV THAKUR <you@example.com>
2026-02-18 07:26:30 +0530

    first

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+a=int(input('Enter the first number'))
+b=int(input('Enter the Second number'))
+add=a+b
+sub=a-b
+mul =a*b
+div=a/b
+print(f'Addition:{add}')
+print(f'Subtraction:{sub}')
+print(f'Multiplication:{mul}')
+print(f'Division:{div}')
